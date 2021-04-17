53cdfe70ab0108031189c47e125a89c565a50b2f
DaveJarvis <Dave.Jarvis@gmail.com>
2021-04-17 12:44:53 -0700

    Add build script to generate archive

---
+++ release.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# ---------------------------------------------------------------------------
+# This script archives all files needed by ConTeXt to typeset a document.
+# Included are a variety of document styles.
+# ---------------------------------------------------------------------------
+
+$log "Create theme packs"
+rm -f theme-packs.zip
+git archive HEAD --prefix=themes/ --format=zip -o theme-packs.zip
+
